116a28e72cc8a6061f45f5a0b1d6167e35883fe5
Amarachi Emmanuel <106779160+Amara2019@users.noreply.github.com>
2023-12-26 02:08:29 +0100

    added to the web page

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Emmanuel Amarachi",
+  title: "Web Developer",
+  email: "emmanuelamarachi55@gmail.com",
+  gitHub: "Amara2019",
+  instagram: "emmanuelamarachi55",
+  linkedIn: "amarachi-emmanuel",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "amra_2022",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
